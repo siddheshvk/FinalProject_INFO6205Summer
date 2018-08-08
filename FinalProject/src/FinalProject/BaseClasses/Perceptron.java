6fbc074e111a7e539687154947b10230953ddd26
Siddhesh Kuvalekar <sid.kuvalekar@gmail.com>
2018-08-08 02:29:36 -0400

    perceptron building

--- FinalProject/src/FinalProject/BaseClasses/Perceptron.java
+++ FinalProject/src/FinalProject/BaseClasses/Perceptron.java
@@ -5,10 +5,76 @@
  */
 package FinalProject.BaseClasses;
 
+import java.util.List;
+
 /**
  *
  * @author Siddhesh
  */
 public class Perceptron {
+    
+    private List<Synapse> inputs;
+//    private ActivationStrategy activationStrategy;
+    private double output;
+    private double derivative;
+    private double weightedSum;
+    private double error;
+
+    public Perceptron(List<Synapse> inputs, double output, double derivative, double weightedSum, double error) {
+        this.inputs = inputs;
+        this.output = output;
+        this.derivative = derivative;
+        this.weightedSum = weightedSum;
+        this.error = error;
+    }
+    
+//    public Perceptron(ActivationStrategy activationStrategy) {
+//        inputs = new ArrayList<Synapse>();
+//        this.activationStrategy = activationStrategy;
+//        error = 0;
+//    }
+
+    public List<Synapse> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(List<Synapse> inputs) {
+        this.inputs = inputs;
+    }
+
+    public double getOutput() {
+        return output;
+    }
+
+    public void setOutput(double output) {
+        this.output = output;
+    }
+
+    public double getDerivative() {
+        return derivative;
+    }
+
+    public void setDerivative(double derivative) {
+        this.derivative = derivative;
+    }
+
+    public double getWeightedSum() {
+        return weightedSum;
+    }
+
+    public void setWeightedSum(double weightedSum) {
+        this.weightedSum = weightedSum;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public void setError(double error) {
+        this.error = error;
+    }
+    
+    
+    
     
 }
